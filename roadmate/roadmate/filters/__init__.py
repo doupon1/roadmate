8251bd839a7bd603e31dd620056c5f60ee09cacc
lluucca <lluucca@hotmail.com>
2008-10-07 10:07:58 +0000

    New custom filter date_for_tables registered. When used, the date is displayed in "Sat 01 Nov 2008" form.

--- roadmate/roadmate/filters/__init__.py
+++ roadmate/roadmate/filters/__init__.py
@@ -3,11 +3,19 @@
 """
 
 import re
+
 from google.appengine.ext import webapp
- 
+
+from django.template.defaultfilters import date
+
 register = webapp.template.create_template_register()
  
-def local_part_from_email(email_address):
-	return re.sub(r"(.*?)@(.*)", r"\1", email_address)
+def local_part_from_email(value):
+	return re.sub(r"(.*?)@(.*)", r"\1", value)
  
 register.filter(local_part_from_email)
+
+def date_for_table(value):
+	return date(value, "D d M Y")
+
+register.filter(date_for_table)
