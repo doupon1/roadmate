9f2bb4c6903b322d27128a232a2c1182512b5d72
dale.halliwell.159356 <dale.halliwell.159356@99fe3840-9752-0410-a3bb-c39d7c806afd>
2008-10-09 10:23:50 +0000

    Added custom filter initialization to base template

--- roadmate/roadmate/handlers/baserequesthandler.py
+++ roadmate/roadmate/handlers/baserequesthandler.py
@@ -7,39 +7,39 @@ from roadmate.models.roadmateuser import RoadMateUser
 class BaseRequestHandler(webapp.RequestHandler):
 	"""
 		RoadMate RequestHandler
-		
-		This page defines the base template for the rest of the pages. 
+
+		This page defines the base template for the rest of the pages.
 		The handlers here are inherited by the handlers for other pages
-		
+
 		Pages:
 			/templates/base.html
 	"""
-	
+
 	def get_request_parameter(self, name, converter=None, default=None):
 		"""docstring for get_request_parameter"""
 		request_value = self.request.get(name)
-		
+
 		if request_value == "":
 			return default
-		
+
 		if converter is not None:
 			try:
 				return converter(request_value)
-				
+
 			except (TypeError, ValueError):
 				return default
-		
+
 		return request_value
-	
+
 	def generate_template_values(self, page_url):
 		"""	Generates a dictionary of template values required by the pages
 			served from this request handler.
-			
+
 			Parameters:
 				page_url - The URL of the page requesting the template values.
-			
+
 		"""
-		
+
 		# --------------------------------------------------------------------
 		# Generate Template Values
 		# --------------------------------------------------------------------
@@ -51,19 +51,22 @@ class BaseRequestHandler(webapp.RequestHandler):
 
 		login_url = users.create_login_url(page_url)
 		logout_url = users.create_logout_url(page_url)
-		
+
 
 		# --------------------------------------------------------------------
 		# Store Template Values
 		# --------------------------------------------------------------------
 		template_values = {}
-		
+
 		template_values['current_user'] = current_user
 		template_values['isLoggedIn'] = isLoggedIn
 		template_values['login_url'] = login_url
 		template_values['logout_url'] = logout_url
 		template_values['page_url'] = page_url
-		
+
 		return template_values
-		
-		
+
+# ----------------------------------------------------------------------------
+#  Load Custom Django Template Filters
+# ----------------------------------------------------------------------------
+webapp.template.register_template_library('roadmate.filters')
