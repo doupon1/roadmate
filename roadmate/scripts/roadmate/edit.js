53683a229a450e82a3d5c07d5dd756fe027c7818
C.Crasborn <C.Crasborn@99fe3840-9752-0410-a3bb-c39d7c806afd>
2008-10-12 04:53:08 +0000

    Updated 'Edit Control' javascript:  - It now enters edit mode by clicking on it, rather than and edit link.

--- roadmate/scripts/roadmate/edit.js
+++ roadmate/scripts/roadmate/edit.js
@@ -1,4 +1,3 @@
-
 // editControl(fieldname, userid)
 // use this function to standardize the creation of all the inplaceeditor controls
 // just a wrapper on the creation of InPlaceEditor
@@ -10,19 +9,15 @@
 // Returns: an instance of Ajax.InPlaceEditor
 //
 function editTextControl(fieldname, handler) {
-	document.write("<a id='edit_" + fieldname + "' href='#'>edit</a>"); //inject the edit link
-	return new Ajax.InPlaceEditor(
+	new Ajax.InPlaceEditor(
 		$(fieldname),
 		handler,
 		{
-			cols: 10,
 			highlightcolor: "#88ac0b",
 			highlightendcolor: "#E8E8E8",
 			okControl: 'link',
 			okText: 'save',
-			cancelControl: 'false',
-			externalControl: 'edit_' + fieldname,
-			externalControlOnly: 'true'
+			cancelControl: 'false'
 		}
 		);
 	};
